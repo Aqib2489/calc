139732748613c2e71554992085676917d2e02609
Aqib2489 <102385918+Aqib2489@users.noreply.github.com>
2022-03-26 06:04:06 +0530

    Update calc updated.py

--- calc updated.py
+++ calc updated.py
@@ -1,22 +1,3 @@
-''' EXAMPLES OF ARITHMETIC OPERATOR
-for checking remainder in dividing of two numbers use - %
-for exponential use - **
-for  flooring in division use - //
-EXAMPLE OF ASSIGNMENT OPERATOR
-X += 5   ( adds 5 to the value of x )
-x /= 5   ( divides 5 to the value of x)
-EXAMPLES OF COMPARISON OPERATOR
- i == 5 (checks whetehr i = 5 or not )
-i!= 5 ( checks whether i is not equal to 5 or it is )
-i >= 5 (checks whether i is greater than 5 or not )
-EXAMPLES OF LOGICAL OPERATOR
-BASICS  [TRUE AND TRUE - TRUE , TRUE AND FALSE - FALSE , TRUE OR FALSE - TRUE, TRUE OR TRUE - TRUE]
-is , is not ( identity operator )
-EXAMPLES OF MEMBERSHIP OPERATOR
-L=[12, 21 ,52 , 69 ,37,32]
-PRINT ( 324 in L) , OUTPUT - TRUE
-EXAMPLES OF BITWISE OPERATOR
-'''
 print(" ____________________________________________________________________________________________________ \n Let's say , \n 1 means add \n 2 means subtract \n 3 means multiply \n 4 means divide \n 5 means exponent")
 def add (x,y):
     return x+y
